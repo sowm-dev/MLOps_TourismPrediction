9a5db622c5b0fffceff28fea057ed4694c84944e
sowm-dev <sowmyaravichandran79@gmail.com>
2026-07-29 20:09:00 +0530

    update tourism_project/deployment/app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -12,25 +12,6 @@ st.write("""
 Predict whether a customer is likely to purchase the Wellness Tourism Package based on demographic and interaction details.
 """)
 
-# Age         = st.number_input(label="Age",min_value=18,max_value=61,value=18,step=1)
-# CityTier     = st.number_input(label="CityTier",min_value=1,max_value=3,value=1,step=1)
-# NumberOfPersonVisiting = st.number_input(label="NumberOfPersonVisiting",min_value=1,max_value=5,value=1,step=1)
-# PreferredPropertyStar    = st.number_input(label="PreferredPropertyStar",min_value=3,max_value=5,value=3,step=1)
-# NumberOfTrips       = st.number_input(label="NumberOfTrips",min_value=1,max_value=22,value=1,step=1)
-# Passport    = st.number_input(label="Passport",min_value=0,max_value=1,value=1,step=1)
-# OwnCar         = st.number_input(label="OwnCar",min_value=0,max_value=1,value=1,step=1)
-# NumberOfChildrenVisiting     = st.number_input(label="NumberOfChildrenVisiting",min_value=0,max_value=3,value=1,step=1)
-# MonthlyIncome    = st.number_input(label="MonthlyIncome",min_value=1000,max_value=89678,value=1,step=1)
-# PitchSatisfactionScore       = st.number_input(label="PitchSatisfactionScore",min_value=1,max_value=5,value=1,step=1)
-# NumberOfFollowups    = st.number_input(label="NumberOfFollowups",min_value=1,max_value=6,value=1,step=1)
-# DurationOfPitch         = st.number_input(label="DurationOfPitch",min_value=5,max_value=127,value=5,step=1)
-# TypeofContact     = st.selectbox("TypeofContact", ["Self Enquiry", "Company Invited"])
-# Occupation = st.selectbox("Occupation", ["Salaried", "Free Lancer", "Small Business","Large Business"])
-# Gender    = st.selectbox("Gender", ["Female", "Male"])
-# ProductPitched       = st.selectbox("ProductPitched", ["Deluxe", "Basic", "Standard","Super Deluxe","King"])
-# MaritalStatus    = st.selectbox("MaritalStatus", ["Single", "Married", "Divorced","Unmarried"])
-# Designation    = st.selectbox("Designation", ["Manager", "Executive", "Senior Manager","AVP","VP"])
-
 
 # -------------------------
 # Numerical Inputs
@@ -191,35 +172,7 @@ input_data["InternationalTraveller"] = (
 ).astype(int)
 
 
-# input_data = pd.DataFrame([{
-#     "Age": Age,
-#     "City Tier": CityTier,
-#     "Number Of PersonVisiting": NumberOfPersonVisiting,
-#     "Preferred Property Star": PreferredPropertyStar,
-#     "Number Of Trips": NumberOfTrips,
-#     "Passport": Passport,
-#     "OwnCar": OwnCar,
-#     "Number Of Children Visiting": NumberOfChildrenVisiting,
-#     "Monthly Income": MonthlyIncome,
-#     "Pitch Satisfaction Score": PitchSatisfactionScore,
-#     "Number Of Followups": NumberOfFollowups,
-#     "Duration Of Pitch": DurationOfPitch,
-#     "Type of Contact": TypeofContact,
-#     "Occupation": Occupation,
-#     "Gender": Gender,
-#     "Product Pitched": ProductPitched,
-#     "Marital Status": MaritalStatus,
-#     "Designation": Designation,
-# }])
-
-# if st.button("Predict Package purchase"):
-#     prediction = model.predict(input_data)[0]
-#     result = "Package purchased" if prediction == 1 else "No Failure"
-#     st.subheader("Prediction Result:")
-#     st.success(f"The model predicts: **{result}**")
-
-
- # -------------------------------------------------
+# -------------------------------------------------
 # Prediction
 # -------------------------------------------------
 
@@ -234,4 +187,4 @@ if st.button("Predict Package Purchase"):
     else:
         st.error(
             "Customer is unlikely to purchase the Wellness Tourism Package."
-        )   
+        )
